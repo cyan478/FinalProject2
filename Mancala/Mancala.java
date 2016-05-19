6d8a0fa1167d1a239bd52f266b5b3e966ffddd19
cyan478 <cyan2@stuy.edu>
2016-05-19 10:15:10 -0400

    finish

--- Mancala/Mancala.java
+++ Mancala/Mancala.java
@@ -1,3 +1,5 @@
+//goes in a circular direction counterclockwise
+
 public class Mancala {
 
     public int [] _red, _blue;
@@ -11,7 +13,7 @@ public class Mancala {
 	    _blue[i] = 4;
 	}
 	_redPit = _bluePit = _hand = 0;
-	_turn = 1;
+`	_turn = 1;
     }
 
     public int size(){
@@ -19,34 +21,63 @@ public class Mancala {
     }
 
     public void turn(int i){
-	int r;
-	if (_turn%2 == 1){
+	if (_turn % 2 == 1){ //ODD -- PLAYER1
 	    //take from the red
-	    r = _red[i];
+	    _hand; = _red[i];
 	    _red[i] = 0;
+	    //if hand == 0 we throw exception
+	    while (hand != 0){
+		addRed(i); //checked if not 0 already
+		if (hand != 0){ //if u reach front of red array
+		    _redPit++; //just drop one in pit
+		    _hand--;
+		}
+		if (hand != 0){ //if you still have more
+		    addBlue(0); //start at blue pit
+		} 
+		if (hand != 0){ //final check 
+		    _bluePit++; //drop one in pit
+		}
+	    } //while loop until your hand is 0 (aka no more marbles)
 	}
-	else{
-	    //take from blue
-	    r = _blue[i];
+	else{ //EVEN -- PLAYER2
+	    //take from the blue
+	    _hand; = _blue[i];
 	    _blue[i] = 0;
-	}
-  	
+	    //if hand == 0 we throw exception
+	    while (hand != 0){
+		addBlue(i); //checked if not 0 already
+		if (hand != 0){ //if u reach end of blue array
+		    _bluePit++; //just drop one in pit
+		    _hand--;
+		}
+
+		//STOPPED HERE 
+		if (hand != 0){ //if you still have more
+		    addRed(_red.length); //start at end of red pit
+		} 
+		if (hand != 0){ //final check 
+		    _bluePit++; //drop one in pit
+		}
+	    } //while loop until your hand is 0 (aka no more marbles)
+  
 	_turn++;
 	System.out.println(this);
     }
 
-    //hi
 
-
-
-    //increment ============================================
-    public void inc(int hand){
-	hand++;
+    public void addRed(int index){
+	for (int i = index; i>0; i--){
+	    _red[i]++;
+	    _hand--;
+	}
     }
 
-    //decrement ============================================
-    public void dec(int hand){
-	hand--;
+    public void addBlue(int index){
+	for (int i = 0; i < index; i++){
+	    _blue[i]++;
+	    _hand--;
+	}
     }
 
     //toString =============================================

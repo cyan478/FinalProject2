447cfb9228958b40f17e954fb7f65e7fb7ffdb50
TheViper9 <mespinosa@stuy.edu>
2016-05-24 16:59:08 -0400

    5/24

--- Connect4/Connect4.java
+++ Connect4/Connect4.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+//use frame??
+
 public class Connect4{
     
     public int _turn;
@@ -19,16 +22,40 @@ public class Connect4{
 	    _bot[i] = 5;
     }
 
+
+
+
+
+    //check for 5 6 and 7
     public void turn(int i){
-	if (i<0||i>6)
-	    return;
-	if (_turn%2 ==1)
-	    _board[_bot[i]][i]=1;
-	else _board[_bot[i]][i] = 2;
-	_bot[i]++;
+	if (i<0||i>6) return;
+	if (_turn%2 ==1){
+	    _board[_bot[i]][i]=1;}
+	else _board[_bot[i]][i]=2;
+	_bot[i]--;
 	_turn++;
+	System.out.println(this);
+    }
+    
+    //backtrack this
+    //i = 1 or i = 2
+    public boolean win(){
+	for(int i =0; i<6;i++){
+	    for(int i = 0; i<7;i++){
+		if (_board[i][j]==0)
+		    continue;
+		else{
+		    
+		}
+	    }
+	}
     }
 
+    public boolean win2(int x, int y, int k){
+	if (if x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
+	    return false;
+	
+    }
     //(potato) chips
     public String toString(){
 	String ans = "";
@@ -43,20 +70,18 @@ public class Connect4{
 		}
 	    }
 	    ans+="\n\n";
-
 	}
 	return ans;
-
     }
 
     public static void main(String [] args){
 	Connect4 a = new Connect4();
 	System.out.println(a+"\n");
 	a.turn(1);
+	a.turn(2);
+	a.turn(3);
 	a.turn(1);
-	a.turn(1);
-	a.turn(1);
-	a.turn(1);
+	a.turn(3);
 	
     }
 

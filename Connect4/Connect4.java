55b699be920d7f4435c0a67e84fa12b1b2c475d8
TheViper9 <mespinosa@stuy.edu>
2016-05-25 16:23:38 -0400

    kill me

--- Connect4/Connect4.java
+++ Connect4/Connect4.java
@@ -29,33 +29,41 @@ public class Connect4{
     //check for 5 6 and 7
     public void turn(int i){
 	if (i<0||i>6) return;
-	if (_turn%2 ==1){
+	int k = _turn%2;
+	if (k==1){
 	    _board[_bot[i]][i]=1;}
 	else _board[_bot[i]][i]=2;
 	_bot[i]--;
 	_turn++;
-	System.out.println(this);
+       	System.out.println(this);
+	if (win(-1*k+2))
+	    System.out.println("GAME OVER");
     }
     
     //backtrack this
     //i = 1 or i = 2
-    public boolean win(){
+    public boolean win(int k){
+	boolean ans = false;
 	for(int i =0; i<6;i++){
-	    for(int i = 0; i<7;i++){
-		if (_board[i][j]==0)
-		    continue;
-		else{
-		    
-		}
+	    for(int j = 0; j<7;j++){
+		if (_board[i][j]==k){
+		    ans = winR(i,j,k,1);
+		if (ans)
+		    return ans;}
 	    }
 	}
+	return ans;
     }
 
-    public boolean win2(int x, int y, int k){
-	if (if x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
+    public boolean winR(int x, int y, int k, int cnt){
+	if (x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
 	    return false;
+	if (cnt >= 4) return true;
+	return (winR(x+1,y,k,cnt+1)||winR(x,y+1,k,cnt+1)||winR(x+1,y+1,k,cnt+1)||winR(x+1,y-1,k,cnt+1));
+     	
 	
     }
+    
     //(potato) chips
     public String toString(){
 	String ans = "";
@@ -81,6 +89,12 @@ public class Connect4{
 	a.turn(2);
 	a.turn(3);
 	a.turn(1);
+	a.turn(3);
+	
+	a.turn(0);
+	a.turn(3);
+	a.turn(0);
+	a.turn(3);
 	a.turn(3);
 	
     }

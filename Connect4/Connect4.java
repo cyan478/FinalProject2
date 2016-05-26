f3d8648024b3212c4b68912348afbf7d3f962a67
TheViper9 <mespinosa@stuy.edu>
2016-05-26 09:16:08 -0400

    hi

--- Connect4/Connect4.java
+++ Connect4/Connect4.java
@@ -10,6 +10,8 @@ public class Connect4{
     //2= $
     //1 goes first
 
+    public static final String HELP = "temp 4 now";
+
     public Connect4(){
 	_board= new int [6][7];
 	_turn = 1;
@@ -18,7 +20,7 @@ public class Connect4{
 	    for (int j = 0; j<7; j++)
 		_board[i][j] = 0;
 	}
-	for (int i=0; i<6; i++)
+	for (int i=0; i<7; i++)
 	    _bot[i] = 5;
     }
 
@@ -30,7 +32,9 @@ public class Connect4{
 
     //check for 5 6 and 7
     public void turn(int i){
-	if (i<0||i>6) return;
+	if (i<0||i>6){ System.out.println("\nInvalid row number. Please enter an integer between 1 and 7, inclusive.\n");
+	    return;
+	}
 	int k = _turn%2;
 	if (k==1){
 	    _board[_bot[i]][i]=1;}
@@ -39,7 +43,7 @@ public class Connect4{
 	_turn++;
        	System.out.println(this);
 	if (win(-1*k+2))
-	    System.out.println("GAME OVER");
+	    System.out.println("GAME OVER: Player "+-1*k+2 +"wins! Press Ctrl+C to exit");
     }
     
     //backtrack this
@@ -58,17 +62,38 @@ public class Connect4{
     }
 
     public boolean winR(int x, int y, int k, int cnt){
+	if (_board[x][y]!=k)
+	    return false;
+	return (win1(x+1,y,k,cnt+1)||win2(x,y+1,k,cnt+1)||win3(x+1,y+1,k,cnt+1)||win4(x+1,y-1,k,cnt+1));	
+    }
+    public boolean win1(int x, int y, int k, int cnt){
 	if (x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
 	    return false;
 	if (cnt >= 4) return true;
-	return (winR(x+1,y,k,cnt+1)||winR(x,y+1,k,cnt+1)||winR(x+1,y+1,k,cnt+1)||winR(x+1,y-1,k,cnt+1));
-     	
-	
+	return win1(x+1,y,k,cnt+1);
+    }
+    public boolean win2(int x, int y, int k, int cnt){
+	if (x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
+	    return false;
+	if (cnt >= 4) return true;
+	return win2(x,y+1,k,cnt+1);
+    }
+    public boolean win3(int x, int y, int k, int cnt){
+	if (x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
+	    return false;
+	if (cnt >= 4) return true;
+	return win2(x+1,y+1,k,cnt+1);
+    }
+    public boolean win4(int x, int y, int k, int cnt){
+	if (x<0||y<0||x>=6||y>=7||_board[x][y]!=k)
+	    return false;
+	if (cnt >= 4) return true;
+	return win2(x+1,y-1,k,cnt+1);
     }
     
     //(potato) chips
     public String toString(){
-	String ans = "";
+	String ans = "\n";
 	for (int i = 0; i< _board.length; i++){
 	    ans += "|";
 	    for(int j =0;j<_board[i].length; j++){
@@ -79,7 +104,7 @@ public class Connect4{
 		else ans+=" $ |";
 		}
 	    }
-	    ans+="\n\n";
+	    ans+="\n";
 	}
 	return ans;
     }
@@ -92,7 +117,8 @@ public class Connect4{
 	    else System.out.print("Player 2: ");
 	    System.out.println("Which column would you like to put down?");
 	    int x = StdIn.readInt();
-	    a.turn(x);
+	    a.turn(x-1);
+	    
 
 
 	}

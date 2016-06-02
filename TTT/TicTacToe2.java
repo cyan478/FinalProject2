bb96694103e18ab9b2a8e6ed914cf99154d3be5e
TheViper9 <mespinosa@stuy.edu>
2016-06-02 09:00:28 -0400

    asdf

---
+++ TTT/TicTacToe2.java
@@ -0,0 +1,18 @@
+public class TicTacToe2{
+
+    public int[] _board;
+    public int _turn;
+    public boolean _won;
+    
+
+    public TicTacToe2(int i){
+	_board = new int[9];
+	_turn = 1;
+	_won = false;
+
+
+    }
+
+
+
+}
